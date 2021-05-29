e5e8fe91c96fb599d212799e986757411fc97e63
Cleyciane Farias <fariascleycianedelima@gmail.com>
2021-05-28 23:17:38 -0500

    Ajustes do modulo 3 e adiçao do modulo 4

---
+++ Modulo 4/csharpexceptions-projetoInicial/csharpexceptions-projetoInicial/ByteBank/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                CarregarContas();
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("CATCH NO MÉTODO MAIN");
+            }
+            
+       
+
+            Console.WriteLine("Execução finalizada. Tecle para sair.");
+            Console.ReadLine();
+        }
+
+        private static void CarregarContas()
+        {
+            using (LeitorDeArquivos leitor = new LeitorDeArquivos("teste.txt"))
+            {
+                leitor.LerProximaLinha();
+            }
+
+            //------------
+            //LeitorDeArquivos leitor = null;
+            //try
+            //{
+            //    leitor = new LeitorDeArquivos("contas1.txt");
+
+            //    leitor.LerProximaLinha();
+            //    leitor.LerProximaLinha();
+            //    leitor.LerProximaLinha();
+                
+            //}
+            //finally
+            //{
+            //    Console.WriteLine("Executando o finally");
+            //    if (leitor != null)
+            //    {
+            //        leitor.Fechar();
+            //    }
+                
+            //}
+
+            
+
+
+        }
+
+        private static void TestaInnerException()
+        {
+            try
+            {
+                ContaCorrente conta1 = new ContaCorrente(4564, 789684);
+                ContaCorrente conta2 = new ContaCorrente(78991, 456794);
+
+                Console.WriteLine("o saldo da conta é " + conta1.Saldo);
+
+                //conta1.Transferir(1000, conta2);
+                conta1.Sacar(10000);
+
+            }
+            catch (OperacaoFinanceiraException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+
+
+                Console.WriteLine("Informações da INNER EXCEPTION (exceção interna)");
+
+                //Console.WriteLine(e.InnerException.Message);
+                //Console.WriteLine(e.InnerException.StackTrace);
+            }
+        }
+
+        // Teste com a cadeia de chamada:
+        // Metodo -> TestaDivisao -> Dividir
+        private static void Metodo()
+        {
+            TestaDivisao(0);
+        }
+
+        private static void TestaDivisao(int divisor)
+        {
+            int resultado = Dividir(10, divisor);
+            Console.WriteLine("Resultado da divisão de 10 por " + divisor + " é " + resultado);
+        }
+
+        private static int Dividir(int numero, int divisor)
+        {
+            try
+            {
+                Console.WriteLine("executando o bloco try");
+                return numero / divisor;
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Deu erro o bloco try");
+                Console.WriteLine("Exceção com numero = " + numero + " e dividor = " + divisor);
+                //controle de fluxo do trecho de código
+                // sai do método e após isso não executa mais nada do trecho de código
+                throw;
+            }
+        }
+    }
+
+}
---
+++ Modulo 4/csharpexceptions-projetoInicial/csharpexceptions-projetoInicial/ByteBank/SaldoInsuficienteException.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    public class SaldoInsuficienteException : OperacaoFinanceiraException
+    {
+        public double Saldo { get; }
+        public double ValorSaque { get; }
+
+        public SaldoInsuficienteException()
+        {
+
+        }
+        public SaldoInsuficienteException(double saldo, double valorSaque)
+            :this("Tentativa de saque do valor de "+ valorSaque +" em uma conta com saldo de "+ saldo)
+        {
+            Saldo = saldo;
+            ValorSaque = valorSaque;
+
+        }
+
+        public SaldoInsuficienteException(string mensagem)
+            : base (mensagem)
+        {
+
+        }
+        // passando varias exceções do inner exception
+        public SaldoInsuficienteException(string mensagem, Exception excecaoInterna)
+            :base(mensagem, excecaoInterna)
+        {
+
+        }
+    }
+}
