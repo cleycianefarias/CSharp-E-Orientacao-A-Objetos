e0cf23d1d5fe38d32f60a0776d66d163b3397287
Cleyciane Farias <fariascleycianedelima@gmail.com>
2021-06-02 12:38:12 -0500

    manipulação de strings em csharp

---
+++ Modulo 6/ByteBank/ByteBank.SistemaAgencia/ExtratorValorDeArgumentosURL.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia
+{
+    public class ExtratorValorDeArgumentosURL
+    {
+        //ctor + tab -> cria método automaticamente
+        //prop + tab cria método get e set automaticamente
+        private readonly string _argumentos;
+        public string URL { get; }
+
+        public ExtratorValorDeArgumentosURL(string url)
+        {
+
+            //string.IsNullOrEmpty(url);
+
+            if (string.IsNullOrEmpty(url))
+            {
+                throw new ArgumentNullException("O argumento url não pode ser nulo ou vazio ",nameof(url));
+            }
+
+            
+
+            int indiceInterrogacao = url.IndexOf('?');
+            _argumentos = url.Substring(indiceInterrogacao + 1);
+
+            if(url == "msngjks")
+            {
+                URL = url;
+            }
+            else
+            {
+               URL = "msngjks" ;
+            }
+
+           
+
+        }
+        //modedaOrigem=real&moedaDestino=dolar
+        public string GetValor(string nomeDoParametro)
+        {
+            return null;
+            //int indiceParametro = _argumentos.IndexOf
+
+        }
+
+
+    }
+}
---
+++ Modulo 6/ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // pagina?argumentos
+            // 012345678
+
+            string textoVazio = "";
+            string textoNulo = null;
+            string textoQualquer = "smkmkdfbnkjsbfhfs";
+
+
+            string palavra = "moedaOrigem=real&moedaDestino=dolar";
+            string nomeArgumento = "moedaDestino";
+
+            int indice = palavra.IndexOf(nomeArgumento);
+            Console.WriteLine("indice: "+indice);
+            Console.WriteLine("Nome do argumento: "+nomeArgumento.Length);
+
+            Console.WriteLine("Tamanho da string nomeArgumento: " + nomeArgumento.Length);
+
+            Console.WriteLine("Palavra: " + palavra);
+            Console.WriteLine("substring: " + palavra.Substring(indice));
+            Console.WriteLine("palavrafinal " + palavra.Substring(indice+nomeArgumento.Length+1));
+
+            Console.WriteLine("Tamanho da string "+ nomeArgumento.Length );
+
+            
+          
+
+            Console.ReadLine();
+
+
+            //testando IsNullOrEmpty
+            //Console.WriteLine(string.IsNullOrEmpty(textoVazio));
+            //Console.WriteLine(string.IsNullOrEmpty(textoNulo));
+            //Console.WriteLine(string.IsNullOrEmpty(textoQualquer));
+            //Console.ReadLine();
+
+
+            ExtratorValorDeArgumentosURL extrator = new ExtratorValorDeArgumentosURL(null);
+
+            string url = "pagina?modedaOrigem=real&moedaDestino=dolar";
+            
+
+            //url += "sufixo";
+
+            int indiceInterrogacao = url.IndexOf('?');
+
+            Console.WriteLine(indiceInterrogacao);
+
+            Console.WriteLine(url);
+            string argumentos = url.Substring(indiceInterrogacao+1);
+            Console.WriteLine(argumentos);
+
+            
+
+            Console.ReadLine();
+
+
+        }
+    }
+
+
+}
