e765ad5be4313382f40bc933050ab309fd1e567d
Cleyciane Farias <fariascleycianedelima@gmail.com>
2021-06-06 18:46:09 -0500

    Ajustes do modulo 6 do curso de CSharp

--- Modulo 6/ByteBank/ByteBank.SistemaAgencia/Program.cs
+++ Modulo 6/ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -32,6 +32,30 @@ namespace ByteBank.SistemaAgencia
             Console.WriteLine(desenvolvedor);
             //sobrecarga contabase
 
+
+            Cliente carlos_1 = new Cliente();
+            carlos_1.Nome = "Carlos";
+            carlos_1.CPF = "456.456.456-65";
+            carlos_1.Profissao = "Design";
+
+            ContaCorrente conta_2 = new ContaCorrente(456, 87654);
+            
+            Cliente carlos_2 = new Cliente();
+            carlos_2.Nome = "Carlos";
+            carlos_2.CPF = "456.456.456-65";
+            carlos_2.Profissao = "Design";
+
+            //comparando objetos primeiro sobescreve o método Equals
+            if (carlos_1.Equals(carlos_2))
+            {
+                Console.WriteLine("São iguais");
+            }
+            else
+            {
+                Console.WriteLine("Não são iguais");
+            }
+
+
             Console.ReadLine();
         }
 
