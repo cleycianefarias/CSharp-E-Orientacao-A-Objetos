d8c8357de1f693cbefffe95b4de10e015c45f5d2
Cleyciane Farias <fariascleycianedelima@gmail.com>
2021-06-14 18:20:14 -0500

    criando um fileStream em Csharp

---
+++ Modulo 9/ByteBankImportacaoExportacao/Program.cs
@@ -0,0 +1,50 @@
+﻿using ByteBankImportacaoExportacao.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO; //IO = input and output
+
+namespace ByteBankImportacaoExportacao 
+{ 
+    class Program 
+    { 
+        static void Main(string[] args) 
+        {
+            //var conta = new ContaCorrente(456,456456);
+            //diretorio de onde está o arquivo
+            var enderecoDoArquivo = "contas.txt";
+            //modo de abertura do documento
+            var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
+            
+            //declarando uma variavel buffer com 1024 bytes
+            //após isso inicializa o valor para leitura do arquivo
+            var buffer = new byte [1024]; //1kb 
+            var numeroDeBytesLidos = -1;
+
+
+            while (numeroDeBytesLidos != 0)
+            {
+                numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+                EscreverBuffer(buffer);   
+            }
+
+            
+            
+
+            Console.ReadLine();
+
+        }
+
+        static void EscreverBuffer(byte[] buffer)
+        {
+            foreach (var meuByte in buffer)
+            {
+                Console.Write(meuByte);
+                Console.Write(" ");
+            }
+        }
+    }
+} 
+ 
