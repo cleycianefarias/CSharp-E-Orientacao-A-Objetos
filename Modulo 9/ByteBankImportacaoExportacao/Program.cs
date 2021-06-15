bd1bc90e454da0cd9173bf86e7811ea90d249787
Cleyciane Farias <fariascleycianedelima@gmail.com>
2021-06-15 10:08:08 -0500

    Usando o streamreader

--- Modulo 9/ByteBankImportacaoExportacao/Program.cs
+++ Modulo 9/ByteBankImportacaoExportacao/Program.cs
@@ -8,37 +8,29 @@ using System.IO; //IO = input and output
 
 namespace ByteBankImportacaoExportacao 
 { 
-    class Program 
+    partial class Program 
     { 
         static void Main(string[] args) 
         {
             var enderecoDoArquivo = "contas.txt";
-          
-            using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
+
+            using (var fluxoDeArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
+            using (var leitor = new StreamReader(fluxoDeArquivo))
             {
-                var buffer = new byte[1024]; //1kb 
-                var numeroDeBytesLidos = -1;
-
-
-                while (numeroDeBytesLidos != 0)
+                while (!leitor.EndOfStream)
                 {
-                    numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
-                    Console.WriteLine($"bytes lidos {numeroDeBytesLidos}");
-                    EscreverBuffer(buffer,numeroDeBytesLidos);
-                
+                    var linha = leitor.ReadLine();
+                    Console.WriteLine(linha);
                 }
             }
-
+               
+            
             Console.ReadLine();
-
         }
 
-        static void EscreverBuffer(byte[] buffer, int bytesLidos)
-        {
-            var utf8 = Encoding.Default;
-            var texto = utf8.GetString(buffer, 0, bytesLidos);
-            Console.Write(texto);
-        }
+
+
+       
     }
 } 
  
