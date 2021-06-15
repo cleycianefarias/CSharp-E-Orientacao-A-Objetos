dc3d708ff4ffe6d3371ced2d497326d9e54d31c3
Cleyciane Farias <fariascleycianedelima@gmail.com>
2021-06-15 12:11:48 -0500

    Ajuste na classe program

--- Modulo 9/ByteBankImportacaoExportacao/Program.cs
+++ Modulo 9/ByteBankImportacaoExportacao/Program.cs
@@ -12,26 +12,8 @@ namespace ByteBankImportacaoExportacao
     { 
         static void Main(string[] args) 
         {
-            //arquivo que já existe
-            var arquivoOriginal = new FileStream("teste.txt", FileMode.Open);
-            //criando novo arquivo
-            var arquivoNovo = new FileStream("teste_copia.txt", FileMode.Create);
-            //armazenando a variavel buffer com 1024 byte
-            var buffer = new byte[1024];
-
-            using (arquivoOriginal)
-            using (arquivoNovo)
-            {
-                var bytesLidos = -1;
-                while (bytesLidos != 0)
-                {
-                    bytesLidos = arquivoOriginal.Read(buffer, 0, 1024);
-                    arquivoNovo.Write(buffer, 0, bytesLidos);
-                }
-            }
-
-            var rodape = Encoding.UTF8.GetBytes("Este documento é uma cópia do original");
-            arquivoNovo.Write(rodape, 0, rodape.Length);
+            CriarArquivoComWriter();
+            Console.WriteLine("Aplicação finalizada!");
 
             Console.ReadLine();
         }
