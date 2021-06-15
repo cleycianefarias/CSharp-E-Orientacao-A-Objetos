4db24045b6d02a2cd98a34fda9b900e4a9477940
Cleyciane Farias <fariascleycianedelima@gmail.com>
2021-06-15 09:39:31 -0500

    Trabalhando com buffers

--- Modulo 9/ByteBankImportacaoExportacao/Program.cs
+++ Modulo 9/ByteBankImportacaoExportacao/Program.cs
@@ -12,38 +12,32 @@ namespace ByteBankImportacaoExportacao
     { 
         static void Main(string[] args) 
         {
-            //var conta = new ContaCorrente(456,456456);
-            //diretorio de onde está o arquivo
             var enderecoDoArquivo = "contas.txt";
-            //modo de abertura do documento
-            var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
-            
-            //declarando uma variavel buffer com 1024 bytes
-            //após isso inicializa o valor para leitura do arquivo
-            var buffer = new byte [1024]; //1kb 
-            var numeroDeBytesLidos = -1;
-
-
-            while (numeroDeBytesLidos != 0)
+          
+            using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
             {
-                numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
-                EscreverBuffer(buffer);   
-            }
+                var buffer = new byte[1024]; //1kb 
+                var numeroDeBytesLidos = -1;
 
-            
-            
+
+                while (numeroDeBytesLidos != 0)
+                {
+                    numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+                    Console.WriteLine($"bytes lidos {numeroDeBytesLidos}");
+                    EscreverBuffer(buffer,numeroDeBytesLidos);
+                
+                }
+            }
 
             Console.ReadLine();
 
         }
 
-        static void EscreverBuffer(byte[] buffer)
+        static void EscreverBuffer(byte[] buffer, int bytesLidos)
         {
-            foreach (var meuByte in buffer)
-            {
-                Console.Write(meuByte);
-                Console.Write(" ");
-            }
+            var utf8 = Encoding.Default;
+            var texto = utf8.GetString(buffer, 0, bytesLidos);
+            Console.Write(texto);
         }
     }
 } 
